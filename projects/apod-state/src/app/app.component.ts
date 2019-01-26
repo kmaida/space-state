45799ed99fbe1bc78eda5f3c37ad53e816ebf2e6
Kim Maida <kim@kmaida.io>
2019-01-26 11:38:09 -0500

    Add star button with count

--- projects/apod-state/src/app/app.component.ts
+++ projects/apod-state/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from './data/apod-data.service';
+import { fromEvent, interval, merge } from 'rxjs';
+import { switchMap, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'apod-root',
@@ -7,10 +9,31 @@ import { DataService } from './data/apod-data.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  @ViewChild('starBtn') starBtn;
+  count = 0;
 
   constructor(public data: DataService) { }
 
   ngOnInit() {
-    this.data.init$().subscribe();
+    this.data.init$().subscribe(
+      apod => this.setupStars()
+    );
+  }
+
+  setupStars() {
+    const click$ = fromEvent(this.starBtn.nativeElement, 'click');
+    const mousedown$ = fromEvent(this.starBtn.nativeElement, 'mousedown');
+    const mouseup$ = fromEvent(this.starBtn.nativeElement, 'mouseup');
+    const mouseleave$ = fromEvent(this.starBtn.nativeElement, 'mouseleave');
+
+    const observer = () => {
+      this.count++;
+    };
+
+    click$.subscribe(observer);
+    const notifier = merge(mouseup$, mouseleave$);
+    const final = mousedown$.pipe(
+      switchMap(e => interval(300).pipe(takeUntil(notifier)))
+    ).subscribe(observer);
   }
 }
